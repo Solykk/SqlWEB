f500c016a6bed96b8c8539b5f5264fb936d59fa6
lyashenko_dmitriy <solykk@yandex.ru>
2017-04-03 22:30:50 +0300

    Delete работает

--- src/main/java/ua/com/juja/sqlweb/control/commands/Insert.java
+++ src/main/java/ua/com/juja/sqlweb/control/commands/Insert.java
@@ -1,11 +1,6 @@
 package ua.com.juja.sqlweb.control.commands;
 
-import ua.com.juja.sqlweb.model.DatabaseManager;
-import ua.com.juja.sqlweb.service.*;
-import ua.com.juja.sqlweb.view.View;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
+import ua.com.juja.sqlweb.service.HelpList;
 
 public class Insert implements Command {
 
@@ -27,46 +22,4 @@ public class Insert implements Command {
         return description;
     }
 
-
-//    @Override
-//    public void process(String command) {
-//
-//        String[] data = correctly.expectedMinEven(command, parametersCount);
-//
-//        String tableName = data[1];
-//        boolean isKey = getKeySet();
-//        ArrayList<String[]> settings = getSettings(data, isKey);
-//
-//        try {
-//            manager.insert(tableName, settings, isKey);
-//            viewService.insertComTry(tableName, command);
-//        } catch (SQLException | NullPointerException e) {
-//            viewService.insertComCatch(tableName, command, e.getMessage());
-//        }
-//    }
-//
-//    private String keyAction() {
-//        view.write("Добавить Seq генератор, если такой имеется? Y/N");
-//        return view.read();
-//    }
-//
-//    private boolean getKeySet() {
-//        String key = keyAction();
-//        return key.equalsIgnoreCase("y");
-//    }
-//
-//    private String seqAction() {
-//        view.write("Введите название колонки: ");
-//        return view.read();
-//    }
-//
-//    private ArrayList<String[]> getSettings(String[] data, boolean isKey) {
-//        ArrayList<String[]> settings = new ArrayList<>();
-//        if(isKey){
-//            String seqName = seqAction();
-//            settings.add(new String[]{seqName, ""});
-//        }
-//        settingsHelper.addSettings(data, settings);
-//        return settings;
-//    }
 }
--- src/main/webapp/delete.jsp
+++ src/main/webapp/delete.jsp
@@ -1,16 +1,47 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Solyk
-  Date: 30.03.2017
-  Time: 19:52
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Delete</title>
 </head>
 <body>
+<form action="Delete" method="post">
+    <table>
+        <tr>
+            <td>Table Name</td>
+            <td><input type="text" name="TableName"/></td>
+        </tr>
+    </table>
 
+    <table>
+        <c:forEach items="${inputVal}" var="inputs">
+            <c:choose>
+                <c:when test="${inputs%2 == 0}">
+                    <tr>
+                        <td>Column Name</td>
+                        <td><input type="text" name="settings[]" /></td>
+                    </tr>
+                </c:when>
+                <c:otherwise>
+                    <tr>
+                        <td>Value</td>
+                        <td><input type="text" name="settings[]" /></td>
+                    </tr>
+                </c:otherwise>
+            </c:choose>
+        </c:forEach>
+        <tr>
+            <td></td>
+            <td><input type="submit" value="Добавить поле" name="add"/></td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <td></td>
+            <td><input type="submit" value="Очистить" name="put"/></td>
+        </tr>
+    </table>
+</form>
+<%@include file="footer.jsp" %>
 </body>
 </html>
